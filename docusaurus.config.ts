4b4d9a8f34af8e3122c5d0ed320a766d417cea79
tp.jp <chronokreuz@gmail.com>
2025-07-30 23:23:00 +0900

    ソーシャルカードの差し替え

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,7 +64,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/drinkable-alcoholic-social-card.png",
     navbar: {
       title: "TpLab",
       logo: {
